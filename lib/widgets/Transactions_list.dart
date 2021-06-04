fd9db70bacfac9d1d8cf2272b46f13aa0036756e
Hamjeth <hamjeth68@gmail.com>
2021-06-04 17:20:04 +0530

    ImagesAdded

--- lib/widgets/Transactions_list.dart
+++ lib/widgets/Transactions_list.dart
@@ -14,58 +14,69 @@ class TansactionsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
+      child: transcactions.isEmpty
+          ? Column(
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 15,
-                    horizontal: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Theme.of(context).primaryColor, width: 2),
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '\$${transcactions[index].ammount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor),
-                  ),
+                Text(
+                  'No Transaction added yet!',
+                  style: Theme.of(context).textTheme.title,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      transcactions[index].title.toString(),
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    Text(
-                      transcactions[index].brandName.toString(),
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(transcactions[index].date),
-                      style: TextStyle(
-                        color: Colors.blueGrey,
-                        fontSize: 11,
-                      ),
-                    ),
-                  ],
-                ),
+                Image.asset('assets/images/waiting.png')
               ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 15,
+                          horizontal: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Theme.of(context).primaryColor, width: 2),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$${transcactions[index].ammount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            transcactions[index].title.toString(),
+                            style: Theme.of(context).textTheme.title,
+                          ),
+                          Text(
+                            transcactions[index].brandName.toString(),
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMd()
+                                .format(transcactions[index].date),
+                            style: TextStyle(
+                              color: Colors.blueGrey,
+                              fontSize: 11,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              itemCount: transcactions.length,
             ),
-          );
-        },
-        itemCount: transcactions.length,
-      ),
     );
   }
 }
