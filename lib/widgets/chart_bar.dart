c112877f02db4538acef3d46ec3fd3f502b3d0cd
Hamjeth <hamjeth68@gmail.com>
2021-06-07 15:08:45 +0530

    BarChart Initiliazed

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-  const ChartBar({Key? key}) : super(key: key);
+  final String label;
+  final double sepndingAmount;
+  final double speingPcOfTotal;
 
+  ChartBar(this.label, this.sepndingAmount, this.speingPcOfTotal);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Text('\$${sepndingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
   }
 }
